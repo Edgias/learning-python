78333cf6e838403b9d2d8f7a2254d23e4d17c54a
Edgias Mudzukwa <mudzukwae@outlook.com>
2023-05-27 22:37:26 +0200

    Scalar Types, Operators, and Control Flow

---
+++ scalartypes_operators_and_controflow/control_flow.py
@@ -0,0 +1,21 @@
+if True:
+    print("It's true!")
+
+if bool("eggs"):
+    print("Yes please!")
+
+if "eggs":
+    print("Yes please!")
+
+h = 42
+if h > 50:
+    print("Greater than 50")
+else:
+    print("50 or smaller")
+
+if h > 50:
+    print("Greater than 50")
+elif h < 20:
+    print("Less than 20")
+else:
+    print("Between 20 and 50")
---
+++ scalartypes_operators_and_controflow/relational_operators.py
@@ -0,0 +1,11 @@
+a = 20
+c = a == 20
+print('value of a is: ', a)
+print('a is equal to 20: ', c)
+print('a is equal to 13: ', a == 13)
+print('a is not equalt to 20: ', a != 20)
+print('a is not equal to 13: ', a != 13)
+print('a is less than 30: ', a < 30)
+print('a is less than or equal to 20: ', a <= 20)
+print('a is greater than 30: ', a > 30)
+print('a is greater than or equal to 30: ', a >= 30)
